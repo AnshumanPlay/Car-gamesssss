be61b3740ad36312a222e613ac557de94762a647
AnshumanPlay <74415215+AnshumanPlay@users.noreply.github.com>
2021-01-07 19:47:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Car-gamesssss
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,15 @@
+var car;
+var wall;
+var speed,weight;
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+speed=random(55,90)
+weight=random(400,1500)
+car
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
